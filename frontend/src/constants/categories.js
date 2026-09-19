35aebc23341ca3f8e3a7daa646774afba84206de
GunjanYadav09 <gunjanyadav927@gmail.com>
2026-09-19 09:38:52 +0530

    feat(student): rebuild issue list with search and filters

---
+++ frontend/src/constants/categories.js
@@ -0,0 +1,11 @@
+export const CATEGORIES = Object.freeze({
+  ELECTRICAL: 'Electrical',
+  PLUMBING: 'Plumbing',
+  FURNITURE: 'Furniture',
+  CLEANLINESS: 'Cleanliness',
+  IT: 'IT',
+  HVAC: 'HVAC',
+  OTHER: 'Other',
+});
+
+export const CATEGORY_OPTIONS = Object.values(CATEGORIES);
